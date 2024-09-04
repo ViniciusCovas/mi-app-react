434fadab16c78843a0c090691cad6b228f1ba297
ViniciusCovas <viniciuscovas@gmail.com>
2024-09-04 15:03:50 -0600

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './rain-bread-sales-dashboard';
+import App from './dashboard';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
